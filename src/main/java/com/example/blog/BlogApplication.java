7266a955ca7c47afff403eb0fcfb596aab20df50
Gary McCart <gwmccart3@gmail.com>
2025-09-04 15:11:22 -0400

    Prepare Heroku deploy

--- src/main/java/com/example/blog/BlogApplication.java
+++ src/main/java/com/example/blog/BlogApplication.java
@@ -1,5 +1,6 @@
 package com.example.blog;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +8,14 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class BlogApplication {
 
 	public static void main(String[] args) {
+		Dotenv dotenv = Dotenv.configure()
+				.ignoreIfMissing() // optional: ignore if .env is missing
+				.load();
+
+		// Set Spring properties manually
+		System.setProperty("spotify.client.id", dotenv.get("SPOTIFY_CLIENT_ID"));
+		System.setProperty("spotify.client.secret", dotenv.get("SPOTIFY_CLIENT_SECRET"));
+		System.setProperty("spotify.redirect.uri", dotenv.get("SPOTIFY_REDIRECT_URI"));
 		SpringApplication.run(BlogApplication.class, args);
 	}
 
--- src/main/java/com/example/blog/s3/AwsConfig.java
+++ src/main/java/com/example/blog/s3/AwsConfig.java
@@ -1,33 +1,19 @@
 package com.example.blog.s3;
 
-import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
-import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.s3.S3Client;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.beans.factory.annotation.Value;
-
 
 @Configuration
 public class AwsConfig {
 
-    @Value("${cloud.aws.credentials.access-key}")
-    private String accessKey;
-
-    @Value("${cloud.aws.credentials.secret-key}")
-    private String secretKey;
-
-    @Value("${cloud.aws.region.static}")
-    private String region;
+    private final String region = "us-east-1"; // or read from properties if you want
 
     @Bean
     public S3Client s3Client() {
         return S3Client.builder()
                 .region(Region.of(region))
-                .credentialsProvider(StaticCredentialsProvider.create(
-                        AwsBasicCredentials.create(accessKey, secretKey)))
-                .build();
+                .build(); // Uses IAM role automatically
     }
 }
-
