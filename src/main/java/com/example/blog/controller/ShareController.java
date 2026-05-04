27972dc4e8625c4aabf7904819113503e53898ce
Gary McCart <gwmccart3@gmail.com>
2026-05-04 15:03:35 -0400

    update share

--- src/main/java/com/example/blog/controller/ShareController.java
+++ src/main/java/com/example/blog/controller/ShareController.java
@@ -14,16 +14,18 @@ public class ShareController {
 
     @GetMapping
     public void shareRedirect(HttpServletRequest request, HttpServletResponse response) throws IOException {
-
+        System.out.println("🔥 SHARE HIT");
         String userAgent = request.getHeader("User-Agent").toLowerCase();
 
         boolean isIOS = userAgent.contains("iphone") || userAgent.contains("ipad");
 
         if (isIOS) {
             // Try to open the iOS app
+            System.out.println("This is IOS");
             response.sendRedirect("trasora://create");
         } else {
             // Web fallback
+            System.out.println("This should redirect to website");
             response.sendRedirect("https://www.trasora.com/create");
         }
     }
